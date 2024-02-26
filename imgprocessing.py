bfbc62b9add97e102acedb36b4334509e18fb258
adamsaleh1112 <144299219+adamsaleh1112@users.noreply.github.com>
2024-02-25 19:14:39 -0500

    commit and push final

--- imgprocessing.py
+++ imgprocessing.py
@@ -4,12 +4,12 @@ import numpy as np
 def processed(img, maskx, masky, maskw, maskh):
 
     # PROCESSING
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.blur(gray, (3, 3))
-    edges = cv2.Canny(blur, 50, 150, apertureSize=3)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converts image to grayscale
+    blur = cv2.blur(gray, (3, 3)) # blurs / smoothens image
+    edges = cv2.Canny(blur, 50, 150, apertureSize=3) # apply acnny edge detection to image which makes edges of objects stand out
 
     # MASK VARIABLES
-    x = maskx
+    x = maskx # mask x, y, w, h
     y = masky
     w = maskw
     h = maskh
@@ -21,4 +21,7 @@ def processed(img, maskx, masky, maskw, maskh):
 
 # PROGRAMMING WORKS CITED
 
-# Masking (Lines 18-25): https://stackoverflow.com/questions/11492214/opencv-via-python-is-there-a-fast-way-to-zero-pixels-outside-a-set-of-rectangle
+# Grayscale (Line 7): https://www.geeksforgeeks.org/python-grayscaling-of-images-using-opencv/
+# Blur (Line 8): https://docs.opencv.org/4.x/d4/d13/tutorial_py_filtering.html
+# Canny (Line 9): https://docs.opencv.org/4.x/da/d22/tutorial_py_canny.html
+# Masking (Lines 12-18): https://stackoverflow.com/questions/11492214/opencv-via-python-is-there-a-fast-way-to-zero-pixels-outside-a-set-of-rectangle
--- main.py
+++ main.py
@@ -11,50 +11,46 @@ while (True): # indefinite loop
 
     # IMAGE PROCESSING
     ret, img = vid.read() # img or (frame) is equal to the frame being read
-
     maskx = 150 # mask variables
     masky = 100
     maskw = 350
     maskh = 250
-
     maskimg = processed(img=img, maskx=maskx, masky=masky, maskw=maskw, maskh=maskh) # image processing function
 
     contours, hierarchy = cv2.findContours(maskimg, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE) # detecting contour lines
 
+    # IF THERE ARE CONTOURS
     if contours is not None and len(contours) >= 2: # if there are atleast two contour lines detected
-
         midline = [] # defining an array for me to store the midpoints values for later
 
         contours = sorted(contours, key=cv2.contourArea, reverse=False) # sorting the contours by area so bigger will be the outer curve and smaller will be the inner curve
         contour1 = contours[0] # smallest (inner) contour
         contour2 = contours[-1] # biggest (outer) contour
-
-        cv2.polylines(img, [np.array(contour1)], False, (255, 0, 0), 3)
-        cv2.polylines(img, [np.array(contour2)], False, (0, 0, 255), 3)
+        cv2.polylines(img, [np.array(contour1)], False, (255, 0, 0), 3) # collects all the points stored in the contour1 array and connects lines in between them
+        cv2.polylines(img, [np.array(contour2)], False, (0, 0, 255), 3) # collects all the points stored in the contour2 array and connects lines in between them
 
         for i in range(min(len(contour1), len(contour2))):
-            xavg = (contour1[i][0][0] + contour2[i][0][0]) / 2
-            xavg = int(xavg)
-            yavg = (contour1[i][0][1] + contour2[i][0][1]) / 2
-            yavg = int(yavg)
+            xavg = (contour1[i][0][0] + contour2[i][0][0]) / 2 # calculates x average by adding an x (depending on iteration or variale i) from both contours and dividing by 2
+            xavg = int(xavg) # turns it into a whole number interger
+            yavg = (contour1[i][0][1] + contour2[i][0][1]) / 2 # calculates y average by adding an y (depending on iteration or variale i) from both contours and dividing by 2
+            yavg = int(yavg) # turns it into a whole number interger
 
-            midline.append((xavg, yavg))
+            midline.append((xavg, yavg)) # appends average x and y as ordered pair to midline array
 
         # POLYLINES CONNECT ARRAY
-        midline = np.array(midline)
-        cv2.polylines(img, [np.array(midline)], False, (0, 255, 0), 3)
+        cv2.polylines(img, [np.array(midline)], False, (0, 255, 0), 3) # collects all the points stored in the midline array and connects lines in between them
 
         # HIGHLIGHT MASK WITH RECTANGLE
-        cv2.rectangle(img, (maskx, masky), (maskx + maskw, masky + maskh), (255, 255, 255), 3)
+        cv2.rectangle(img, (maskx, masky), (maskx + maskw, masky + maskh), (255, 255, 255), 3) # uses the variables from the rectangle mask to draw a rectangle for end user purposes
 
         # SHOW IMAGE
-        cv2.imshow('frame', img)
+        cv2.imshow('frame', img) # uses cv2 show image function to display final image
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     else:
-        cv2.imshow('frame', img)
+        cv2.imshow('frame', img) # uses cv2 show image function to display final image
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
@@ -62,8 +58,8 @@ while (True): # indefinite loop
 # PROGRAMMING WORKS CITED
 
 # Video capture and video display (Lines 1-13, 55-63) https://docs.opencv.org/4.x/dd/d43/tutorial_py_video_display.html
-# OpenCV rectangle (Line 48) https://www.geeksforgeeks.org/python-opencv-cv2-rectangle-method/
-# OpenCV contours (Lines 22) https://docs.opencv.org/4.x/d4/d73/tutorial_py_contours_begin.html
-# Contour area (Line 28) https://docs.opencv.org/4.x/dd/d49/tutorial_py_contour_features.html
-# Polylines (Line 45) https://www.geeksforgeeks.org/python-opencv-cv2-polylines-method/
-# Sorting (Line 28) https://docs.python.org/3/howto/sorting.html
+# OpenCV rectangle (Line 44) https://www.geeksforgeeks.org/python-opencv-cv2-rectangle-method/
+# OpenCV contours (Lines 20) https://docs.opencv.org/4.x/d4/d73/tutorial_py_contours_begin.html
+# Contour area (Line 26) https://docs.opencv.org/4.x/dd/d49/tutorial_py_contour_features.html
+# Polylines (Line 29, 30, 41) https://www.geeksforgeeks.org/python-opencv-cv2-polylines-method/
+# Sorting (Line 26) https://docs.python.org/3/howto/sorting.html
